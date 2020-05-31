4daca179456de0e397e3e702e18c8d6a74eb353d
Vova Pytsyuk <vovawed@gmail.com>
2020-05-31 21:22:11 +0300

    Added game over text

--- Classes/GameOver.cpp
+++ Classes/GameOver.cpp
@@ -12,24 +12,27 @@ Scene *GameOver::createScene() {
     return scene;
 }
 
-void GameOver::GoToGameScene(cocos2d::Ref *pSender)
-{
+void GameOver::GoToGameScene(cocos2d::Ref *pSender) {
     auto scene = HelloWorld::createScene();
     Director::getInstance()->replaceScene(scene);
 }
-void GameOver::GoToMainMenu(cocos2d::Ref *pSender)
-{
+
+void GameOver::GoToMainMenu(cocos2d::Ref *pSender) {
     auto scene = MainMenu::createScene();
     Director::getInstance()->replaceScene(scene);
 }
 
 
-
-bool GameOver::init()
-{
-    if (!Layer::init())
-    {
+bool GameOver::init() {
+    if (!Layer::init()) {
         return false;
     }
+
+    Size size = Director::getInstance()->getWinSize();
+
+    auto gameNameLabel = Label::createWithTTF("Game Over", "fonts/Marker Felt.ttf", 36);
+    gameNameLabel->setPosition(Vec2(size.width / 2, size.height / 1.5));
+    this->addChild(gameNameLabel);
+
     return true;
 }
